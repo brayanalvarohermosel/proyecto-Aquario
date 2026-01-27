a3c6394b445fb490c1c99a47db8c8e553dde492c
Brayan <balvarohermosel@iessonferrer.net>
2026-01-27 19:51:53 +0100

    27/01/26

---
+++ src/app/services/fish-service/fish-service.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fish-service',
+  imports: [],
+  templateUrl: './fish-service.html',
+  styleUrl: './fish-service.css',
+})
+export class FishService {
+
+}
